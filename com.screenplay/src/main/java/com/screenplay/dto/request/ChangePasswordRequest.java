ec917acf5fd6d29e14b5a1267fb049580843b070
MANMOHAN4 <manmohanca42005@gmail.com>
2026-03-17 08:03:19 +0530

    feat: create request classes

---
+++ com.screenplay/src/main/java/com/screenplay/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,15 @@
+package com.screenplay.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current Password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New Password is required")
+    private String newPassword;
+}
---
+++ com.screenplay/src/main/java/com/screenplay/enums/Role.java
@@ -0,0 +1,6 @@
+package com.screenplay.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
