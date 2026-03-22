7da469ede6db1499c01fe255344640ccb9283b7c
MANMOHAN4 <manmohanca42005@gmail.com>
2026-03-22 19:53:24 +0530

    refactor: update

--- com.screenplay/src/main/java/com/screenplay/security/JwtAuthenticationFilter.java
+++ com.screenplay/src/main/java/com/screenplay/security/JwtAuthenticationFilter.java
@@ -24,7 +24,7 @@ import java.util.Collections;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
-    private final JwtUtil jwtUtil;
+    private JwtUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
