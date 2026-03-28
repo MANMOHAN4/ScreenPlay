4cf184cfa8a262e4c0ea9bc2441e2994f69a453b
MANMOHAN4 <manmohanca42005@gmail.com>
2026-03-28 10:30:34 +0530

    feat: add ensureNotLastActiveAdmin method

--- com.screenplay/src/main/java/com/screenplay/dao/UserRepository.java
+++ com.screenplay/src/main/java/com/screenplay/dao/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.screenplay.entity.User;
+import com.screenplay.enums.Role;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
@@ -14,5 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByVerificationToken(String verificationToken);
 
     Optional<User> findByPasswordResetToken(String passwordResetToken);
+
+    long countByRoleAndActive(Role role, boolean active);
 
 }
