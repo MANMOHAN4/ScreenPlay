aa480ef4e46cd9788d5cd9b3ee008e839554ff09
MANMOHAN4 <manmohanca42005@gmail.com>
2026-03-28 10:48:31 +0530

    fix: import Pageable properly

--- com.screenplay/src/main/java/com/screenplay/dao/UserRepository.java
+++ com.screenplay/src/main/java/com/screenplay/dao/UserRepository.java
@@ -3,9 +3,9 @@ package com.screenplay.dao;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.screenplay.entity.User;
@@ -22,10 +22,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     long countByRoleAndActive(Role role, boolean active);
 
-    @Query("SELECT u FROM User u WHERE"
-            + "LOWER(u. fullName) LIKE LOWER(CONCAT('%' ,:search,'%')) OR "
-            + "LOWER(u.email) LIKE LOWER(CONCAT('%',:search,'%'))")
-
+    @Query("SELECT u FROM User u WHERE " +
+            "LOWER(u.fullName) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+            "LOWER(u.email) LIKE LOWER(CONCAT('%', :search, '%'))")
     Page<User> searchUsers(@Param("search") String search, Pageable pageable);
 
 }
