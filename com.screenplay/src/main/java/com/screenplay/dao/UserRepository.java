dce1f84fc2958de01834b7514dd1c87171fd3c11
MANMOHAN4 <manmohanca42005@gmail.com>
2026-04-10 18:36:10 +0530

    feat: create methods and queries required to implement video services

--- com.screenplay/src/main/java/com/screenplay/dao/UserRepository.java
+++ com.screenplay/src/main/java/com/screenplay/dao/UserRepository.java
@@ -1,6 +1,8 @@
 package com.screenplay.dao;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,5 +30,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Page<User> searchUsers(@Param("search") String search, Pageable pageable);
 
     long countByRole(Role role);
+
+    @Query("SELECT v.id FROM User u JOIN u.watchList v WHERE u.email = :email AND v.id IN :videoIds")
+    Set<Long> findWatchlistVideoIds(@Param("email") String email, @Param("videoIds") List<Long> videoIds);
 
 }
