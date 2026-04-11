85bf78c283c82fa2d3b303dfea59cea11efe7e94
MANMOHAN4 <manmohanca42005@gmail.com>
2026-04-11 21:50:25 +0530

    fix: frontend url

--- com.screenplay/src/main/java/com/screenplay/serviceImpl/EmailServiceImpl.java
+++ com.screenplay/src/main/java/com/screenplay/serviceImpl/EmailServiceImpl.java
@@ -21,7 +21,7 @@ public class EmailServiceImpl implements EmailService {
     private JavaMailSender mailSender;
 
     // Frontend URL (Angular app)
-    @Value("${app.frontend.url:http://localhost:4200}")
+    @Value("${app.frontend.url:http://localhost:5173}")
     private String frontendUrl;
 
     // Sender email (configured in application.properties)
